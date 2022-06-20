0868fbf37837e1ef5dc59c3082a6b5cb4195a5c9
bakaiskiy <bakaiakylbekov266@gmail.com>
2022-06-20 20:16:06 +0600

    api

--- src/contexts/productsContext.js
+++ src/contexts/productsContext.js
@@ -3,7 +3,8 @@ import axios from "axios";
 
 export const productsContext = React.createContext();
 
-const API = "http://localhost:8000/data";
+// const API = "http://localhost:8000/data";
+const API = "https://makers-deploy-bakai.herokuapp.com/data";
 
 const INIT_STATE = {
   products: [],
